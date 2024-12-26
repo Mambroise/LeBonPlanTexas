397b5fd787e3c318195317b2dfbc21746ded2162
Maurice Ambroise <maurice.ambroise79@gmail.com>
2024-12-26 15:00:32 +0100

    ajout footer

--- static/appmain/js/script.js
+++ static/appmain/js/script.js
@@ -41,19 +41,16 @@ function addStyles(selector) {
         ${selector} {
             transition: height 0.3s ease-in-out;
         }
-
         ${selector}.small {
             height: 100px;
             transition: height 0.5s ease-in-out;
-            // background: linear-gradient(135deg, rgb(47, 80, 212), rgb(30, 33, 165), rgb(35, 72, 219));
             background: linear-gradient(135deg,rgb(67, 111, 58),rgb(55, 89, 48),rgb(67, 111, 58));
+            border-bottom: 1px solid var(--black-color);
         }
-
         #who.small, #register.small {
             font-size: 12px;
             transition:  0.3s ease-in-out;
         }
-
         .nav-register-box.small, .nav-who-box.small {
             margin-top: 40px;
         }
@@ -75,7 +72,7 @@ function addStyles(selector) {
             transition:  0.3s ease-in-out;
         }
         .contact-box.small {
-            margin-top: 3%;
+            margin-top: 2%;
             width: 30%;
             transition:  0.3s ease-in-out;
         }
@@ -83,12 +80,7 @@ function addStyles(selector) {
             font-size: 20px;
             transition:  0.3s ease-in-out;
         }
-        .white-flag-div.small {
-            border: 1px solid var(--blue-texas);
-        }
-        .red-flag-div.small {
-            border: 1px solid var(--red-texas);
-        }
+
     `;
     const styleTag = document.createElement('style');
     styleTag.innerHTML = style;
@@ -106,9 +98,6 @@ const title = document.querySelector('.title')
 const titleSpan = document.querySelector('.title span')
 const titleBox = document.querySelector('.title-box')
 const contactBox = document.querySelector('.contact-box')
-const redFlag = document.querySelector('.red-flag-div')
-const whiteFlag = document.querySelector('.white-flag-div')
-
 
 
 window.addEventListener('scroll', () => {
@@ -123,9 +112,7 @@ window.addEventListener('scroll', () => {
         titleSpan.classList.add('small');
         titleBox.classList.add('small');
         contactBox.classList.add('small');
-        redFlag.classList.add('small');
-        whiteFlag.classList.add('small');
-        
+
     } else {
         navbar.classList.remove('small');
         who.classList.remove('small');
@@ -137,7 +124,6 @@ window.addEventListener('scroll', () => {
         titleSpan.classList.remove('small');
         titleBox.classList.remove('small');
         contactBox.classList.remove('small');
-        redFlag.classList.remove('small');
-        whiteFlag.classList.remove('small');
+
     }
 });
