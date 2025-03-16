82a1a4135e262ec3b445105109f86dd94b4644d7
Maurice Ambroise <maurice.ambroise79@gmail.com>
2025-03-17 00:52:15 +0100

    refactorisation price_view, creation price_service

--- appmain/views/index_view.py
+++ appmain/views/index_view.py
@@ -8,10 +8,18 @@
 
 from django.shortcuts import render
 from ..models import FileForImage,ImageDisplayTheme
+from ..services import PriceService
 
 
 def index(request):
     theme_displayed = ImageDisplayTheme.objects.get(is_active=True)
     files = FileForImage.objects.filter(is_active=True)
+    auto, driver, plat = PriceService.get_active_prices()
 
-    return render(request, 'lebonplantexas/index.html',{'file_names':files,'theme_displayed':theme_displayed})
+    context = {
+        'file_names':files,
+        'theme_displayed':theme_displayed,
+        'auto':auto,
+    }
+
+    return render(request, 'lebonplantexas/index.html',context)
--- appmain/views/service_view.py
+++ appmain/views/service_view.py
@@ -8,26 +8,15 @@
 
 from django.shortcuts import render
 
-from ..models import Price
+from ..services import PriceService
 
 def service_view(request):
     # preparing package availability context for html columns
-    prices = Price.objects.all()
-    if prices:
-        auto = None
-        driver = None
-        plat = None
-        for price in prices:
-            if price.service_name == 'autonome' and price.is_active == True:
-                auto = price
-            elif price.service_name == 'chauffeur privé' and price.is_active == True:
-                driver = price
-            elif price.service_name == 'platinium' and price.is_active == True:
-                plat = price
+    auto, driver, plat = PriceService.get_active_prices()
 
-        context = {
-            'auto':auto,
-            'driver':driver,
-            'plat':plat,
-        }
+    context = {
+        'auto':auto,
+        'driver':driver,
+        'plat':plat,
+    }
     return render(request,'lebonplantexas/services.html',context)
