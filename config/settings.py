2088b589df0d1f6510990db77f61419d0aeffac7
Maurice Ambroise <maurice.ambroise79@gmail.com>
2025-03-28 19:19:09 +0100

    migration bddd vers postgresql

--- config/settings.py
+++ config/settings.py
@@ -108,10 +108,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lebonplantexas',
+        'USER': 'morice',
+        'PASSWORD': 'maurilebonplantexas2312!',
+        'HOST': 'localhost',  # Ou l'IP de ton serveur PostgreSQL
+        'PORT': '5432',
     }
 }
 
